b81f740635db3d3c52de9000a45e24bcf9212326
Anne Christy <achristy@hmc.edu>
2015-11-11 22:02:13 -0800

    Added the ability to pull JSON data down using a GET request.

--- app/src/main/java/com/example/annie/slackproject/MainActivity.java
+++ app/src/main/java/com/example/annie/slackproject/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.annie.slackproject;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +24,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        new GetAPIInfo().execute();
+
+
     }
 
     @Override
@@ -49,4 +53,67 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    class GetAPIInfo extends AsyncTask<Void, Void, String> {
+        private Exception exception;
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        TextView responseView = (TextView) findViewById(R.id.responseView);
+
+        protected void onPreExecute() {
+
+            progressBar.setVisibility(View.VISIBLE);
+            responseView.setText("Response View Text");
+        }
+
+        protected String doInBackground(Void... urls) {
+
+            //String email = findViewById(R.id.emailText).getText().toString();
+            // Do some validation here
+
+            try {
+                URL url = new URL("https://slack.com/api/users.list?token=xoxp-5048173296-5048346304-5180362684-7b3865");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+
+
+            /*try {
+                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+                name = object.getString("name");
+            } catch (JSONException e){
+                exception.printStackTrace();
+            }*/
+
+            responseView.setText(response);
+
+        }
+    }
+
+
 }
